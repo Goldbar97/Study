f2daa77f21cd26fb9738bdba7e4f257e84bae31b
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-04-22 12:57:34 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.34 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] result = new int[score.length];
+        
+        for (int i = 0; i < score.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(score[i]);
+            } else if (pq.peek() < score[i]) {
+                pq.offer(score[i]);
+            }
+            
+            if (pq.size() <= k) {
+                result[i] = pq.peek();
+            } else {
+                pq.poll();
+                result[i] = pq.peek();
+            }
+        }
+        
+        return result;
+    }
+}
