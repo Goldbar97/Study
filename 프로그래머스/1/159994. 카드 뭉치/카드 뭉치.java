0e736fffebcb422151fa5665cb2441204991d291
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-04-23 08:56:05 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        
+        int top1 = 0;
+        int top2 = 0;
+        
+        for (int i = 0; i < goal.length; i++) {
+            String word = goal[i];
+            
+            if (top1 < cards1.length && cards1[top1].equals(word)) {
+                top1++;
+            } else if (top2 < cards2.length && cards2[top2].equals(word)) {
+                top2++;
+            } else {
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+}
