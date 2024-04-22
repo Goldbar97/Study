95170351e3830456ca5107aa4a5f3d1400c0fcfb
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-04-22 12:20:50 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i];
+            
+            for (int j = 0; j < count / 2; j++) {
+                sb.append(i);
+            }
+        }
+        String normal = sb.toString();
+        String reversed = sb.reverse().toString();
+        sb.insert(0, 0).insert(0, normal);
+        
+        return sb.toString();
+    }
+}
