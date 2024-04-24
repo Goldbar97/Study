9fe720d7ede8412bddeee7390528ef1dcd4e4eb2
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-04-24 19:39:52 +0900

    [level 1] Title: 덧칠하기, Time: 1.20 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int count = 0;
+        boolean[] peeled = new boolean[n + 1];
+        
+        for (int s : section) {
+            peeled[s] = true;
+        }
+        
+        
+        for (int i = 1; i <= n; i++) {
+            if (peeled[i]) {
+                count++;
+                i += m - 1;
+            }
+        }
+        
+        return count;
+    }
+}
