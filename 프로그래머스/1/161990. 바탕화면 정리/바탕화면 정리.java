0d8c1f2b104eca2cffb98c97cda625f1db9b80b0
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-05-13 21:23:47 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.24 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,27 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] wallpaper) {
-        ArrayList<int[]> list = new ArrayList<>();
         int[] result;
-        int minRow, maxRow, minCol, maxCol;
+        int minRow = Integer.MAX_VALUE;
+        int maxRow = Integer.MIN_VALUE;
+        int minCol = Integer.MAX_VALUE;
+        int maxCol = Integer.MIN_VALUE;
         
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
                 if (wallpaper[i].charAt(j) == '#') {
-                    list.add(new int[]{i, j});
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
                 }
             }
         }
-        
-        int lastIndex = list.size() - 1;
-        
-        list.sort((x, y) -> x[0] - y[0]);
-        minRow = list.get(0)[0];
-        maxRow = list.get(lastIndex)[0];
-        
-        list.sort((x, y) -> x[1] - y[1]);
-        minCol = list.get(0)[1];
-        maxCol = list.get(lastIndex)[1];
         
         result = new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
         
