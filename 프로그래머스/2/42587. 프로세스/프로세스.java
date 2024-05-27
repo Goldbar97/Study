f31d2bb80944d75d0c9409eb33b2a9962edab607
Goldbar97 <100333239+Goldbar97@users.noreply.github.com>
2024-05-27 20:30:43 +0900

    [level 2] Title: 프로세스, Time: 0.53 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,28 +3,32 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        Queue<Map.Entry<Character, Integer>> queue = new LinkedList<>();
-        SortedMap<Character, Integer> map = new TreeMap<>();
-
-        char target = (char) ('A' + location);
-        int max = 0;
+        Queue<Map.Entry<Integer, Integer>> queue = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
+        int max = 0;
         for (int i = 0; i < priorities.length; i++) {
-            map.put((char) ('A' + i), priorities[i]);
+            map.put(i, priorities[i]);
             max = Math.max(max, priorities[i]);
+        }
+        
+        for (Map.Entry<Integer, Integer> item : map.entrySet()) {
+            queue.offer(item);
         }
         
         int count = 1;
         while (!queue.isEmpty()) {
-            Map.Entry<Character, Integer> item = queue.poll();
+            Map.Entry<Integer, Integer> item = queue.poll();
             
             if (item.getValue() == max) {
-                if (item.getKey() == target) {
+                if (item.getKey() == location) {
                     break;
                 } else {
                     count++;
+                    max = 0;
                 }
-                for (Map.Entry<Character, Integer> i : queue) {
+                
+                for (Map.Entry<Integer, Integer> i : queue) {
                     max = Math.max(max, i.getValue());
                 }
             } else {
